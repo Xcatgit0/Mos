67ae71c0028aa54d4a579316c680f7c9abc74232
Xcatgit0 <163300721+Xcatgit0@users.noreply.github.com>
2024-07-18 18:34:27 +0700

    error

--- lib/application.lua
+++ lib/application.lua
@@ -2,11 +2,11 @@ local application = {}
 local img = require("Image")
 local config = require("config")
 local lang = require("Lang")
-local application.list = {}
+local list = {}
 function application.create(name, sizeX, sizeY, x,y, LangPath)
     
 end
-function application.list.create(name,sizeX,sizeY,x,y)
-     application.list[name] = {}
+function list.create(name,sizeX,sizeY,x,y)
+     list[name] = {}
 end
 return application
