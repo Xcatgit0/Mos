b701a29d8f60ddce8a899f2f3772c5450290f7ac
Xcatgit0 <163300721+Xcatgit0@users.noreply.github.com>
2024-07-19 16:23:43 +0700

    bug fixed

--- lib/application.lua
+++ lib/application.lua
@@ -4,7 +4,12 @@ local config = require("config")
 local lang = require("Lang")
 application.list = {}
 function application.create(name, sizeX, sizeY, x,y, LangPath)
-    local create = load("return {name = "..name..",LangPath="..LangPath..",x="..x..",y="..y..",Data={}}")
-    application.list[name] = create()
+    application.list[name] = {}
+    application.list[name].name = name
+    application.list[name].x = x
+    application.list[name].y = y
+    application.list[name].sizeX = sizeX
+    application.list[name].sizeY = sizeY
+    application.list[name].Data = {}
 end
 return application
