e479b0192b22b338ec37459b81f87d7db7557282
Xcatgit0 <163300721+Xcatgit0@users.noreply.github.com>
2024-07-19 16:07:06 +0700

    fix bug

--- lib/application.lua
+++ lib/application.lua
@@ -2,8 +2,9 @@ local application = {}
 local img = require("Image")
 local config = require("config")
 local lang = require("Lang")
-local list = {}
+application.list = {}
 function application.create(name, sizeX, sizeY, x,y, LangPath)
-    
+    local create = load("return {name = "..name..",LangPath="..LangPath..",x="..x..",y="..y..",Data={}}")
+    application.list[name] = create()
 end
 return application
